707b06daabed2894aafaabeeabd03405b297a688
A. Botorabi <a.botorabi@avm.de>
2018-08-31 16:14:01 +0200

    Improved the device widgets

--- src/main/ngapp/src/app/view-switch-devices/view-switch-devices-component.ts
+++ src/main/ngapp/src/app/view-switch-devices/view-switch-devices-component.ts
@@ -2,11 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import {ApiDeviceService} from "../service/api-device.service";
 import {Device} from "../service/device";
 import {AppInformationService} from "../service/app-information.service";
+import {animate, style, transition, trigger} from "@angular/animations";
+
 
 @Component({
   selector: 'app-view-switch-devices',
   templateUrl: './view-switch-devices-component.html',
-  styleUrls: ['./view-switch-devices-component.css']
+  styleUrls: ['./view-switch-devices-component.css'],
+  animations: [
+    trigger('detailsFading', [
+      transition('void => *', [
+        style({height: '0'}),
+        animate(150, style({height: '*'}))
+      ]),
+      transition('* => void', [
+        style({height: '*'}),
+        animate(250, style({height: '0'}))
+      ])
+    ])
+  ]
 })
 export class ViewSwitchDevicesComponent implements OnInit {
 
@@ -14,6 +28,7 @@ export class ViewSwitchDevicesComponent implements OnInit {
   public error;
 
   private UPDATE_INTERVAL = 30 * 1000;
+  private collapsedWidgets = new Map<string, boolean>();
 
   constructor(private apiDeviceService: ApiDeviceService,
               private appInfoService: AppInformationService) { }
@@ -37,6 +52,7 @@ export class ViewSwitchDevicesComponent implements OnInit {
         this.error = errorStatus;
       }
       else {
+        //! TODO update the devices in a way that the widgets do not get reset!
         this.devices = devices;
         this.appInfoService.setSwitchDeviceCount(this.devices.length);
       }
@@ -63,7 +79,7 @@ export class ViewSwitchDevicesComponent implements OnInit {
     this.apiDeviceService.deviceSwitch(device.id, !device.on, () => {
       setTimeout(() => {
         this.updateDevices();
-      }, 1000);
+      }, 100);
     });
   }
 
@@ -72,6 +88,35 @@ export class ViewSwitchDevicesComponent implements OnInit {
   }
 
   public getTitleClass(name: string) {
-     return name.length > 12 ? "title-small" : "title-normal";
+     return name.length > 9 ? "title-small" : "title-normal";
+  }
+
+  public getDeviceSummary(device: Device) {
+    if (!device.present) {
+      return "Device Not Available!";
+    }
+
+    let summary = "";
+    summary += device.on ? "On" : "Off";
+    if (device.on) {
+      summary += " / " + this.getPower(device) + " Watt";
+    }
+    summary += " / " + this.getTemperature(device) + "°C";
+    return summary;
+  }
+
+  public isWidgetCollapse(device): boolean {
+    return this.collapsedWidgets.has(device.ain) ?
+      this.collapsedWidgets.get(device.ain) : true;
+  }
+
+  public onToggleWidgetCollapse(device): boolean {
+    let collapsed = this.isWidgetCollapse(device);
+    this.collapsedWidgets.set(device.ain, !collapsed);
+    return !collapsed;
+  }
+
+  public getWidgetCollapseSymbol(device: Device) {
+    return this.isWidgetCollapse(device) ? "keyboard_arrow_down" : "keyboard_arrow_up";
   }
 }
